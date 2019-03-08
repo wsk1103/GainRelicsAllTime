8f448914b0e6efc689873227f414e200c55c6a9c
wsk <1261709167@qq.com>
2019-03-08 18:24:27 +0800

    战斗胜利后，必定获得遗物。 After the victory of the battle, you are sure to gain one relic. GitHub：https://github.com/wsk1103/GainRelicsAllTime

---
+++ src/main/java/com/wsk/gain/Gain.java
@@ -0,0 +1,58 @@
+package com.wsk.gain;
+
+import basemod.BaseMod;
+import basemod.interfaces.*;
+import com.evacipated.cardcrawl.modthespire.lib.SpireInitializer;
+
+import static basemod.DevConsole.logger;
+
+/**
+ * @author wsk1103
+ * @date 2019/3/8
+ * @description 描述
+ */
+@SpireInitializer
+public class Gain implements PostInitializeSubscriber,
+        EditCharactersSubscriber, EditCardsSubscriber,
+        EditRelicsSubscriber, EditStringsSubscriber,
+        EditKeywordsSubscriber {
+    public Gain() {
+        BaseMod.subscribe(this);
+    }
+
+    public static void initialize() {
+        logger.info("=========================初始化角色Mod数据=========================");
+        new Gain();//初始化角色mod，必备。
+        logger.info("===========================角色Mod初始化成功===========================");
+    }
+
+    @Override
+    public void receiveEditCards() {
+
+    }
+
+    @Override
+    public void receiveEditCharacters() {
+
+    }
+
+    @Override
+    public void receiveEditKeywords() {
+
+    }
+
+    @Override
+    public void receiveEditRelics() {
+
+    }
+
+    @Override
+    public void receiveEditStrings() {
+
+    }
+
+    @Override
+    public void receivePostInitialize() {
+
+    }
+}
---
+++ src/main/java/com/wsk/gain/GainRelicsAllTime.java
@@ -0,0 +1,46 @@
+package com.wsk.gain;
+
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.relics.AbstractRelic;
+import com.megacrit.cardcrawl.rewards.RewardItem;
+import com.megacrit.cardcrawl.rooms.MonsterRoomBoss;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * @author wsk1103
+ * @date 2019/3/8
+ * @description 描述
+ */
+public class GainRelicsAllTime {
+
+    public void receiveRewards(ArrayList<RewardItem> rewards) {
+        boolean hasRelic = false;
+        if (AbstractDungeon.getCurrRoom() instanceof MonsterRoomBoss) {
+            return;
+        }
+        for (RewardItem reward : rewards) {
+            if (reward.type == RewardItem.RewardType.RELIC) {
+                hasRelic = true;
+                break;
+            }
+        }
+        if (!hasRelic) {
+            Random random = new Random();
+            int r = random.nextInt(100);
+            AbstractRelic.RelicTier tier = AbstractRelic.RelicTier.COMMON;
+            if (r >= 91) {
+                tier = AbstractRelic.RelicTier.BOSS;
+            } else if (r >= 80) {
+                tier = AbstractRelic.RelicTier.RARE;
+            } else if (r >= 65) {
+                tier = AbstractRelic.RelicTier.UNCOMMON;
+            } else if (r >= 45) {
+                tier = AbstractRelic.RelicTier.SHOP;
+            }
+            rewards.add(new RewardItem(AbstractDungeon.returnRandomRelic(tier)));
+        }
+
+    }
+}
