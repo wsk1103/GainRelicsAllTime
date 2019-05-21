77cd1e185bded5041a73feeaf1df6d947762ec7a
wsk <1261709167@qq.com>
2019-05-21 23:31:47 +0800

    战斗胜利后，必定获得遗物和金卡。 After the victory of the battle, you are sure to gain one relic. GitHub：https://github.com/wsk1103/GainRelicsAllTime

--- src/main/java/com/wsk/gain/GainRelicsAllTime.java
+++ src/main/java/com/wsk/gain/GainRelicsAllTime.java
@@ -6,8 +6,7 @@ import com.megacrit.cardcrawl.relics.AbstractRelic;
 import com.megacrit.cardcrawl.rewards.RewardItem;
 import com.megacrit.cardcrawl.rooms.MonsterRoomBoss;
 
-import java.util.ArrayList;
-import java.util.Random;
+import java.util.*;
 
 /**
  * @author wsk1103
@@ -15,6 +14,8 @@ import java.util.Random;
  * @description 根据概率性，向奖励界面加入一个随机稀有度的遗物和3张金卡。
  */
 public class GainRelicsAllTime {
+
+    private static Queue<AbstractCard> cardList = null;
 
     public void receiveRewards(ArrayList<RewardItem> rewards) {
         if (AbstractDungeon.getCurrRoom() instanceof MonsterRoomBoss) {
@@ -39,11 +40,18 @@ public class GainRelicsAllTime {
         }
         rewards.add(new RewardItem(AbstractDungeon.returnRandomRelic(tier)));
         //加入金卡
+        if (cardList == null || cardList.size() < 3) {
+            cardList = new PriorityQueue<>();
+            List<AbstractCard> randomCard = AbstractDungeon.rareCardPool.group;
+            //乱序一下
+            Collections.shuffle(randomCard);
+            cardList.addAll(randomCard);
+        }
         RewardItem item = new RewardItem(AbstractCard.CardColor.COLORLESS);
         ArrayList<AbstractCard> cards = item.cards;
         cards.clear();
         for (int i = 0; i < 3; i++) {
-            cards.add(AbstractDungeon.getCard(AbstractCard.CardRarity.RARE));
+            cards.add(cardList.poll());
         }
         rewards.add(item);
     }
